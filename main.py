1da7de0f7bbdd60fb64982dfae2e872e59b91908
Ian Agpawa <agpawaji@gmail.com>
2018-03-10 08:50:22 -0500

    feat: Get json and write file for playlists

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+import requests
+import json
+
+playlists_url = "http://ec2-34-201-35-166.compute-1.amazonaws.com/playlists/JSON/"
+
+r = requests.get(playlists_url)
+playlists_json = r.json()
+playlists_decoded = json.dumps(playlists_json)
+
+file = open("test.txt", "w")
+file.write(playlists_decoded)
+file.close()
+
+# base_url = "http://ec2-34-201-35-166.compute-1.amazonaws.com/%s/JSON"
