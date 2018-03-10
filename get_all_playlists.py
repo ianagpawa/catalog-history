a001f49d4348fa94ca8344f3d67db4d82513d8b1
Ian Agpawa <agpawaji@gmail.com>
2018-03-10 09:48:34 -0500

    feat: Add create folder if missing

--- get_all_playlists.py
+++ get_all_playlists.py
@@ -1,17 +1,21 @@
 import sys
+import os
 import requests
 import json
 from time import gmtime, strftime
 
 timestamp = strftime("%Y%b%d-%H%M%S", gmtime())
-print timestamp
 
-playlists_url = "http://ec2-34-201-35-166.compute-1.amazonaws.com/playlists/JSON/"
+# playlists_url = "http://ec2-34-201-35-166.compute-1.amazonaws.com/playlists/JSON/"
+#
+# r = requests.get(playlists_url)
+# playlists_json = r.json()
+# playlists_decoded = json.dumps(playlists_json)
 
-r = requests.get(playlists_url)
-playlists_json = r.json()
-playlists_decoded = json.dumps(playlists_json)
-
-file = open("test.txt", "w")
-file.write(playlists_decoded)
-file.close()
+folder_location = "archive/playlists"
+if not os.path.exists(folder_location):
+    os.mkdir(folder_location)
+file_location = "%s/%s.txt" % (folder_location, timestamp)
+# file = open(file_location, "w")
+# file.write(playlists_decoded)
+# file.close()
