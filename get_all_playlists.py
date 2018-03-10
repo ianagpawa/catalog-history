e35930a57309495175bf82363c57d072e88a2a8d
Ian Agpawa <agpawaji@gmail.com>
2018-03-10 09:53:40 -0500

    feat: Add print statements to get_all_playlists for console output

--- get_all_playlists.py
+++ get_all_playlists.py
@@ -6,16 +6,22 @@ from time import gmtime, strftime
 
 timestamp = strftime("%Y%b%d-%H%M%S", gmtime())
 
-# playlists_url = "http://ec2-34-201-35-166.compute-1.amazonaws.com/playlists/JSON/"
-#
-# r = requests.get(playlists_url)
-# playlists_json = r.json()
-# playlists_decoded = json.dumps(playlists_json)
+playlists_url = "http://ec2-34-201-35-166.compute-1.amazonaws.com/playlists/JSON/"
+
+print "Getting JSON"
+r = requests.get(playlists_url)
+playlists_json = r.json()
+playlists_decoded = json.dumps(playlists_json)
+print "Received JSON"
 
 folder_location = "archive/playlists"
 if not os.path.exists(folder_location):
+    print "Creating %s folder" % folder_location
     os.mkdir(folder_location)
+
 file_location = "%s/%s.txt" % (folder_location, timestamp)
-# file = open(file_location, "w")
-# file.write(playlists_decoded)
-# file.close()
+print "Writing file"
+file = open(file_location, "w")
+file.write(playlists_decoded)
+file.close()
+print "Finished. Created '%s'" % file_location
